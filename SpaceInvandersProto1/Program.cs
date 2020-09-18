9be0cfa4efed643c5a65b35c0f4cc07c3d3ed571
Jimmy Olsson <93jimols@mk.se>
2020-09-18 15:31:45 +0200

    Add project files.

---
+++ SpaceInvandersProto1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace SpaceInvandersProto1
+{
+    class Program
+
+    {
+        enum State
+        {
+            Menu,
+            Settings,
+            Gameplay,
+            High_Score,
+            Exit,
+        }
+        enum DifficultLevel
+        {
+            Low,
+            Medium,
+            High,
+        }
+        static void Main(string[] args)
+        {
+            State screen = State.Menu;
+
+            Console.WriteLine("-----Start Menu-----");
+            Console.WriteLine("");
+            Console.WriteLine("\n 1: Play Game");
+            Console.WriteLine("\n 2: Settings");
+            Console.WriteLine("\n 3: High Scores");
+            Console.WriteLine("\n 4: Exit");
+
+            Console.WriteLine("Waiting for input$ ");
+            Console.SetCursorPosition(0, Console.WindowLeft);
+            string input = Console.ReadLine();
+
+            //input check
+            byte choice;
+            bool found_term = Byte.TryParse(input, out choice);
+            while (!found_term || !(choice >= 1 && choice <= 4))
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Not a valid input");
+                Console.Write("Enter first term: ");
+                found_term = Byte.TryParse(Console.ReadLine(), out choice);
+            }
+
+            if (choice >= 1 && choice <= 4)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        screen = State.Gameplay;
+                        break;
+                    case 2:
+                        screen = State.Settings;
+                        break;
+                    case 3:
+                        screen = State.High_Score;
+                        break;
+                    case 4:
+                        screen = State.Exit;
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            Console.WriteLine(screen);
+        }
+    }
+}
---
+++ SpaceInvandersProto1/Settings.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceInvandersProto1
+{
+
+    public static class Difficulty
+    {
+        public static int SetHP(int input)
+        {
+
+            int hp = 10;
+            switch (input)
+            {
+                case 1:
+                    hp = 10;
+                    break;
+                case 2:
+                    hp = 15;
+                    break;
+                case 3:
+                    hp = 20;
+                    break;
+                default:
+                    break;
+            }
+            return hp;
+        }
+        public static int SetEnemyNumber(int input)
+        {
+
+            int enemyNumber = 10;
+            switch (input)
+            {
+                case 1:
+                    enemyNumber = 10;
+                    break;
+                case 2:
+                    enemyNumber = 15;
+                    break;
+                case 3:
+                    enemyNumber = 20;
+                    break;
+                default:
+                    break;
+            }
+            return enemyNumber;
+        }
+
+        class Enemies
+        {
+            int health = Difficulty.SetHP(1);
+        }
+
+        public static int SetDifficulty(int input)
+        {
+
+            int DifficultySetting = 1;
+            switch (input)
+            {
+                case 1:
+                    DifficultySetting = 1;
+                    break;
+                case 2:
+                    DifficultySetting = 2;
+                    break;
+                case 3:
+                    DifficultySetting = 3;
+                    break;
+                default:
+                    DifficultySetting = 2;
+                    break;
+            }
+            return DifficultySetting;
+        }
+
+    }
+}
